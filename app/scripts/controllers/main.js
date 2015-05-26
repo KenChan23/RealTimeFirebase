9805ad3547071d04accb95a9365a51a30f9cb2ba
Ken Chan <KChan923@gmail.com>
2015-05-26 02:38:19 -0400

    added real time updating of message field in text field

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the demoApp
  */
 angular.module('demoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $timeout) {
     // $scope.awesomeThings = [
     //   'HTML5 Boilerplate',
     //   'AngularJS',
@@ -18,6 +18,26 @@ angular.module('demoApp')
     var childRef = rootRef.child("message");
     // var childRef = rootRef.child('message');
     // var parentRef = childRef.parent();
+    
+    childRef.on('value', function(snapshot){
+      $timeout(function(){
+        var snapshotVal = snapshot.val();
+        console.log(snapshot);
+        console.log(snapshotVal);
+        $scope.message = snapshotVal;
+      });
+    });
+
+    $scope.$watch('message.text', function(newVal){
+      if(!newVal){
+        return;
+      }
+      childRef.update({
+        text: newVal
+      });
+      console.log(newVal);
+    });
+
     $scope.setMessage = function(){
       childRef.set({
         user: 'Bob',
